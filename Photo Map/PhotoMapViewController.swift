28f2dd1553cb4eca46723283abc187d246c798bb
Chau Vo <minhchau273@gmail.com>
2016-07-18 10:32:37 +0700

    [f] Milestone 2: Create the Map View

--- Photo Map/PhotoMapViewController.swift
+++ Photo Map/PhotoMapViewController.swift
@@ -7,29 +7,19 @@
 //
 
 import UIKit
+import MapKit
 
 class PhotoMapViewController: UIViewController {
+
+  @IBOutlet weak var mapView: MKMapView!
 
   override func viewDidLoad() {
     super.viewDidLoad()
 
-    // Do any additional setup after loading the view.
+    //one degree of latitude is approximately 111 kilometers (69 miles) at all times.
+    let sfRegion = MKCoordinateRegionMake(CLLocationCoordinate2DMake(37.783333, -122.416667),
+                                          MKCoordinateSpanMake(0.1, 0.1))
+    mapView.setRegion(sfRegion, animated: false)
   }
-
-  override func didReceiveMemoryWarning() {
-    super.didReceiveMemoryWarning()
-    // Dispose of any resources that can be recreated.
-  }
-
-
-  /*
-   // MARK: - Navigation
-
-   // In a storyboard-based application, you will often want to do a little preparation before navigation
-   override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-   // Get the new view controller using segue.destinationViewController.
-   // Pass the selected object to the new view controller.
-   }
-   */
 
 }
